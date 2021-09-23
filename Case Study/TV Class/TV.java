dca62b76ee29079c74c0a5afe660eefa03a45382
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-09-23 16:16:29 +0900

    Create TV.java

---
+++ Case Study/TV Class/TV.java
@@ -0,0 +1,40 @@
+public class TV {
+	int channel = 1; // states
+	int volumeLevel = 1;
+	boolean on = false;
+	
+	public TV() { // behaviors
+	}
+	
+	public void turnOn() {
+		on = true;
+	}
+	
+	public void turnOff() {
+		on = false;
+	}
+	
+	public void setChannel(int newChannel) {
+		if (on && newChannel >= 1 && newChannel <= 120) channel = newChannel;
+	}
+	
+	public void setVolume(int newVolumeLevel) {
+		if (on && newVolumeLevel >= 1 && newVolumeLevel <= 7) volumeLevel = newVolumeLevel;
+	}
+	
+	public void channelUp() {
+		if(on && channel < 120) channel++;
+	}
+	
+	public void channelDown() {
+		if(on && channel > 1) channel--;
+	}
+	
+	public void volumeUp() {
+		if(on && volumeLevel < 7) volumeLevel++;
+	}
+	
+	public void volumeDown(){
+		if(on && volumeLevel > 1) volumeLevel--;
+	}
+}
