3dbf17302e2ec82479a3343a2cb6570eb96a7f2b
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-11-25 20:51:02 +0900

    Update Clock.java

--- Case Study/Clock with FX/Clock.java
+++ Case Study/Clock with FX/Clock.java
@@ -8,16 +8,16 @@ import javafx.scene.layout.BorderPane;
 
 public class Clock extends Application{
 	public void start(Stage primaryStage){
-		ClockPane clock = new ClockPane();
-		String timeString = clock.getHour() + ":" + clock.getMinute() + ":" + clock.getSecond();
-		Label lblCurrentTime = new Label(timeString);
+		ClockPane clock = new ClockPane(); // Pane에 시계 모양을 그리기 위한 ClockPane Class는 따로 선언한다.
+		String timeString = clock.getHour() + ":" + clock.getMinute() + ":" + clock.getSecond(); // 이는 현재 시간을 받아 시계 아래에 표시해줄 String.
+		Label lblCurrentTime = new Label(timeString); // 현재 시간을 담는 Label 키워드이다.
 		
-		BorderPane pane = new BorderPane();
-		pane.setCenter(clock);
-		pane.setBottom(lblCurrentTime);
-		BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+		BorderPane pane = new BorderPane(); // 위, 아래, 오른쪽, 중앙에 Control을 배치하는 레이아웃이다.
+		pane.setCenter(clock); // 중앙의 Control을 Clock으로
+		pane.setBottom(lblCurrentTime); // 아래의 Control을 현재 시간으로.
+		BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER); // 줄을 맞추도록 setAlignment로 정렬시킨다.
 		
-		Scene scene = new Scene(pane, 250, 250);
+		Scene scene = new Scene(pane, 250, 250); // Scene에는 250, 250 크기의 Pane이 들어간다.
 		primaryStage.setTitle("Clock");
 		primaryStage.setScene(scene);
 		primaryStage.show();
