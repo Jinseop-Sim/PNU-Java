dffd2fa36fcf58b59d4ad283ad702d1d5dbfe5f4
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-11-25 20:46:48 +0900

    Create Clock.java

---
+++ Case Study/Clock with FX/Clock.java
@@ -0,0 +1,28 @@
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class javafxtext extends Application{
+	public void start(Stage primaryStage){
+		ClockPane clock = new ClockPane();
+		String timeString = clock.getHour() + ":" + clock.getMinute() + ":" + clock.getSecond();
+		Label lblCurrentTime = new Label(timeString);
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(clock);
+		pane.setBottom(lblCurrentTime);
+		BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+		
+		Scene scene = new Scene(pane, 250, 250);
+		primaryStage.setTitle("Clock");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
